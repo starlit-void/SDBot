d34cbd8bc835af8aa87ec4abe63130e30a0918c4
starlit <chasmaticvoid@gmail.com>
2020-06-27 18:57:57 +0300

    add events for player join and player quit

--- src/main/java/com/ihusker/sdbot/Source.java
+++ src/main/java/com/ihusker/sdbot/Source.java
@@ -1,11 +1,11 @@
 package com.ihusker.sdbot;
 
-import com.google.common.base.Charsets;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.BufferedReader;
@@ -23,69 +23,161 @@ import java.util.concurrent.Executors;
 
 public class Source extends JavaPlugin implements Listener {
 
-    private URL url;
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+	private URL url;
+	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    public void onEnable() {
-        saveDefaultConfig();
+	public void onEnable() {
+		saveDefaultConfig();
 
-        String url = getConfig().getString("webhook");
-        if (url != null) {
-            try {
-                this.url = new URL(url);
-            } catch (MalformedURLException e) {
-                getLogger().warning("You must provide a valid url for the Webhook.");
-                getServer().getPluginManager().disablePlugin(this);
-                return;
-            }
-        }
+		String url = getConfig().getString("webhook");
+		if (url != null) {
+			try {
+				this.url = new URL(url);
+			} catch (MalformedURLException e) {
+				getLogger().warning("You must provide a valid url for the Webhook.");
+				getServer().getPluginManager().disablePlugin(this);
+				return;
+			}
+		}
 
-        getServer().getPluginManager().registerEvents(this, this);
-    }
+		getServer().getPluginManager().registerEvents(this, this);
+	}
 
-    @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
-        Player player = event.getPlayer();
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
 
-        executorService.execute(() -> {
-            HttpURLConnection connection;
-            try {
-                connection = (HttpURLConnection) url.openConnection();
+		executorService.execute(() -> {
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
 
-                try {
-                    connection.setRequestMethod("POST");
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                }
+				try {
+					connection.setRequestMethod("POST");
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				}
 
-                connection.setRequestProperty("Content-Type", "application/json; utf-8");
-                connection.setRequestProperty("Accept", "application/json");
-                connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
-                connection.setDoOutput(true);
+				connection.setRequestProperty("Content-Type", "application/json; utf-8");
+				connection.setRequestProperty("Accept", "application/json");
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+				connection.setDoOutput(true);
 
-                Charset charset = StandardCharsets.UTF_8;
-                try (OutputStream outputStream = connection.getOutputStream()) {
-                    byte[] input = (
-                            "{\"username\":\"" + player.getName() +
-                                    "\",\"content\":\"" + event.getMessage() +
-                                    "\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
-                    ).getBytes(charset);
-                    outputStream.write(input, 0, input.length);
-                }
+				Charset charset = StandardCharsets.UTF_8;
+				try (OutputStream outputStream = connection.getOutputStream()) {
+					byte[] input = (
+							"{\"username\":\"" + player.getName() +
+									"\",\"content\":\"" + "_" + event.getQuitMessage() + "_" +
+									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
+					).getBytes(charset);
+					outputStream.write(input, 0, input.length);
+				}
 
-                if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
-                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
-                    String inputLine;
-                    StringBuilder response = new StringBuilder();
-                    while ((inputLine = bufferedReader.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    getLogger().info(response.toString());
-                }
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
+					String inputLine;
+					StringBuilder response = new StringBuilder();
+					while ((inputLine = bufferedReader.readLine()) != null) {
+						response.append(inputLine);
+					}
+					getLogger().info(response.toString());
+				}
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-    }
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+
+		executorService.execute(() -> {
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				try {
+					connection.setRequestMethod("POST");
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				}
+
+				connection.setRequestProperty("Content-Type", "application/json; utf-8");
+				connection.setRequestProperty("Accept", "application/json");
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+				connection.setDoOutput(true);
+
+				Charset charset = StandardCharsets.UTF_8;
+				try (OutputStream outputStream = connection.getOutputStream()) {
+					byte[] input = (
+							"{\"username\":\"" + player.getName() +
+									"\",\"content\":\"" + "_" + event.getJoinMessage() + "_" +
+									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
+					).getBytes(charset);
+					outputStream.write(input, 0, input.length);
+				}
+
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
+					String inputLine;
+					StringBuilder response = new StringBuilder();
+					while ((inputLine = bufferedReader.readLine()) != null) {
+						response.append(inputLine);
+					}
+					getLogger().info(response.toString());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player player = event.getPlayer();
+
+		executorService.execute(() -> {
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				try {
+					connection.setRequestMethod("POST");
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				}
+
+				connection.setRequestProperty("Content-Type", "application/json; utf-8");
+				connection.setRequestProperty("Accept", "application/json");
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+				connection.setDoOutput(true);
+
+				Charset charset = StandardCharsets.UTF_8;
+				try (OutputStream outputStream = connection.getOutputStream()) {
+					byte[] input = (
+							"{\"username\":\"" + player.getName() +
+									"\",\"content\":\"" + event.getMessage() +
+									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
+					).getBytes(charset);
+					outputStream.write(input, 0, input.length);
+				}
+
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
+					String inputLine;
+					StringBuilder response = new StringBuilder();
+					while ((inputLine = bufferedReader.readLine()) != null) {
+						response.append(inputLine);
+					}
+					getLogger().info(response.toString());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
 }
