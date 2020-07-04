75a4d8d947dce286dcb494249f641040ccd561e7
starlit <chasmaticvoid@gmail.com>
2020-07-04 15:27:52 +0300

    add death messages too

--- src/main/java/com/ihusker/sdbot/Source.java
+++ src/main/java/com/ihusker/sdbot/Source.java
@@ -1,8 +1,10 @@
 package com.ihusker.sdbot;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -41,6 +43,52 @@ public class Source extends JavaPlugin implements Listener {
 		}
 
 		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event) {
+		Entity player = event.getEntity();
+
+		executorService.execute(() -> {
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				try {
+					connection.setRequestMethod("POST");
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				}
+
+				connection.setRequestProperty("Content-Type", "application/json; utf-8");
+				connection.setRequestProperty("Accept", "application/json");
+				connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11");
+				connection.setDoOutput(true);
+
+				Charset charset = StandardCharsets.UTF_8;
+				try (OutputStream outputStream = connection.getOutputStream()) {
+					byte[] input = (
+							"{\"username\":\"" + player.getName() +
+									"\",\"content\":\"" + "_" + player.getName() + " died at " + player.getLocation() + "._ " + event.getDeathMessage() +
+									"\",\"avatar_url\":\"https://crafatar.com/renders/head/" + player.getUniqueId() + "?overlay\"}"
+					).getBytes(charset);
+					outputStream.write(input, 0, input.length);
+				}
+
+				if (connection.getResponseCode() != HttpURLConnection.HTTP_CREATED) return;
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), charset))) {
+					String inputLine;
+					StringBuilder response = new StringBuilder();
+					while ((inputLine = bufferedReader.readLine()) != null) {
+						response.append(inputLine);
+					}
+					getLogger().info(response.toString());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 	@EventHandler
